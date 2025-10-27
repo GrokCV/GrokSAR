e456376cf4991a55b46343f5de5e1cc05717fb97
MinruiZou <traveler_wood@163.com>
2025-10-27 11:49:51 +0800

    Upload the Script for Displaying Precise COCO Metric

--- groksar/evaluation/metrics/__init__.py
+++ groksar/evaluation/metrics/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dota_big2small_metric import DotaBig2SmallMetric
 from .VOC_show_metric import VOCShowMetric
+from .COCO_show_metric import CocoShowMetric
 
-__all__ = ['DotaBig2SmallMetric','VOCShowMetric']
+__all__ = ['DotaBig2SmallMetric','VOCShowMetric', 'CocoShowMetric']
